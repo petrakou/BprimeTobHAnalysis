8e90345bcd0e54b55d4f4f9d1cdca70a793ffb6c
Devdatta <devdatta.majumder@cern.ch>
2013-10-29 20:29:42 +0100

    Cummulative dist function updated

--- bin/histCummulative.cc
+++ bin/histCummulative.cc
@@ -5,12 +5,16 @@ int main (int argc, char *argv[]) {
   TH1D* hgaus = new TH1D ("hgaus", "gaus", 1000, -50, 50) ; 
   hgaus->Sumw2() ; 
   hgaus->FillRandom("gaus", 10000) ; 
-  TGraphAsymmErrors* gerf = getCummulative(hgaus) ; 
+  char optUp[10] = "Up" ;
+  TGraphAsymmErrors* gerfUp = getCummulative(hgaus, optUp) ; 
+  char optDown[10] = "Down" ;
+  TGraphAsymmErrors* gerfDown = getCummulative(hgaus, optDown) ; 
 
   TFile * fout = new TFile("file.root","RECREATE") ; 
   fout->cd() ; 
   hgaus->Write();
-  gerf->Write();
+  gerfUp->Write();
+  gerfDown->Write();
   fout->Close() ;
 
   return 0 ; 
--- interface/histCummulative.h
+++ interface/histCummulative.h
@@ -1,37 +1,80 @@
 #include <TH1F.h>
 #include <TH1D.h>
+#include <TString.h> 
 #include <TGraphAsymmErrors.h>
 #include <TFile.h>
+
 #include <cmath> 
+#include <iostream>
 
 using namespace std ; 
 
 template <class Type> 
-TGraphAsymmErrors* getCummulative (Type histp) {
+TGraphAsymmErrors* getCummulative (const Type histp, const char* option) {
+
+  int nbins(histp->GetNbinsX());
+
+  double val1(histp->GetBinContent(nbins+1));
+  double val2(histp->GetBinContent(nbins));
+  double err1(histp->GetBinError(nbins+1));
+  double err2(histp->GetBinError(nbins));
+  histp->SetBinContent(nbins+1, 0);
+  histp->SetBinContent(nbins, val1 + val2);
+  histp->SetBinError(nbins, sqrt(err1*err1 + err2*err2));
+
+  double Val1(histp->GetBinContent(nbins+1));
+  double Val2(histp->GetBinContent(nbins));
+  double Err1(histp->GetBinError(nbins+1));
+  double Err2(histp->GetBinError(nbins));
+  histp->SetBinContent(0, 0);
+  histp->SetBinContent(1, Val1 + Val2);
+  histp->SetBinError(1, sqrt(Err1*Err1 + Err2*Err2));
 
   Type hcummulp = (Type)histp->Clone("hcummul") ; 
   hcummulp->Sumw2() ; 
   hcummulp->Reset(0) ; 
 
+  TString mode(option) ; 
   double val(0) ; 
   double err(0) ; 
-  for (int ibin = 0; ibin <= histp->GetNbinsX()+1; ++ibin) {
-    val += histp->GetBinContent(ibin) ; 
-    err = (err*err) + (histp->GetBinError(ibin)*histp->GetBinError(ibin)) ; 
-    err = sqrt(err) ; 
-    hcummulp->SetBinContent(ibin, val) ; 
-    hcummulp->SetBinError(ibin, err) ; 
+  for (int ibin = 1; ibin <= nbins; ++ibin) {
+    if (mode.Contains("Up")) {
+      val += histp->GetBinContent(ibin) ; 
+      err = (err*err) + (histp->GetBinError(ibin)*histp->GetBinError(ibin)) ; 
+      err = sqrt(err) ; 
+      hcummulp->SetBinContent(ibin, val) ; 
+      hcummulp->SetBinError(ibin, err) ; 
+    }
+    else if (mode.Contains("Down")) {
+      val += histp->GetBinContent(nbins + 1 - ibin) ; 
+      err = (err*err) + (histp->GetBinError(nbins + 1 - ibin)*histp->GetBinError(nbins + 1 - ibin)) ; 
+      err = sqrt(err) ; 
+      hcummulp->SetBinContent(nbins + 1 - ibin, val) ; 
+      hcummulp->SetBinError(nbins + 1 - ibin, err) ; 
+    }
+    else {
+      std::cout << " Error: Option " << option << " not recognised\n" ; 
+    }
   }
 
   Type htot = (Type)histp->Clone("htot") ; 
   htot->Sumw2() ; 
-  for (int ibin = 0; ibin <= histp->GetNbinsX()+1; ++ibin) {
-    htot->SetBinContent(ibin, hcummulp->GetBinContent(hcummulp->GetNbinsX()+1)) ; 
-    htot->SetBinError(ibin, hcummulp->GetBinError(hcummulp->GetNbinsX()+1)) ; 
+  for (int ibin = 1; ibin <= nbins; ++ibin) {
+    if (mode.Contains("Up")) {
+      htot->SetBinContent(ibin, hcummulp->GetBinContent(nbins)) ; 
+      htot->SetBinError(ibin, hcummulp->GetBinError(nbins)) ; 
+    }
+    else if (mode.Contains("Down")) {
+      htot->SetBinContent(nbins + 1 - ibin, hcummulp->GetBinContent(1)) ; 
+      htot->SetBinError(nbins + 1 - ibin, hcummulp->GetBinError(1)) ; 
+    }
+    else {
+      std::cout << " Error: Option " << option << " not recognised\n" ; 
+    }
   }
 
   TGraphAsymmErrors* geff = new TGraphAsymmErrors(hcummulp, htot, "cl=0.683 b(1,1) mode") ; 
-  geff->SetName("geff") ; 
+  geff->SetName("geff"+mode) ; 
 
   return geff ; 
 }
